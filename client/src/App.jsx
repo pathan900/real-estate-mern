ed3ba9eb9d48b6a56e468ec7f8046dcc2af9d2bf
Abdul <abdulkaleemkhan1999@gmail.com>
2023-12-30 17:42:47 +0530

    initial frontend setup

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<Signin />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,38 @@
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="bg-slate-200">
+      <div className="flex justify-between items-center mx-auto max-w-6xl p-3">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-500">Royal</span>
+            <span className="text-slate-700">Estate</span>
+          </h1>
+        </Link>
+        <form className="bg-slate-100 p-3 rounded-xl flex items-center">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+          />
+          <FaSearch className="text-slate-700" />
+        </form>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700">Home</li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-700">About</li>
+          </Link>
+          <Link to="/sign-in">
+            <li className="sm:inline text-slate-700">Sign in</li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
